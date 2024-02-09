a52af2d296274565f2477e3723124b9a22051c1c
badohoun <espoir.badohoun@gmail.com>
2024-02-09 13:43:31 +0100

    use find_packages from setuptools to find all folder contains __init__.py and insert it inside in your package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup 
+from setuptools import setup , find_packages
 
 setup(
     name='packaging',
     version='0.0.0',
-    packages=['packaging'],
+    packages=find_packages(),
     # include any additional files needed by the package
     include_package_data=True,
     # specify the package metadata
