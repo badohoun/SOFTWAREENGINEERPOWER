cd360aec49a77b937dd053bd284c72bdcbb151bb
badohoun <espoir.badohoun@gmail.com>
2024-02-19 08:55:25 +0100

    publish your package : deliver your package to end user in pypitest and pypiprod

--- packaging_demo/colorized_demo.py
+++ packaging_demo/colorized_demo.py
@@ -1,7 +1,7 @@
 try:
-    from rich import print 
+    from rich import print
 except ImportError:
-    ... 
+    ...
 
 print("Salut: 1,2,10")
-print({"valeur":2, "variable":6})
+print({"valeur": 2, "variable": 6})
